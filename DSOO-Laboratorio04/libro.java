3a0b24bbf273dbd1a6418843c9e6a05590ddd179
RManriquesu <rmanriquesu@unsa.edu.pe>
2025-10-05 18:20:11 -0500

    Avance 1-limpio

--- DSOO-Laboratorio04/libro.java
+++ DSOO-Laboratorio04/libro.java
@@ -1,3 +1,42 @@
 class libro{
-    
+    private String titulo;
+    private String autor;
+    private String ISBN;
+    private boolean disponible;
+
+    public Libro() { 
+        this.titulo = "Sin título";
+        this.autor = "Desconocido";
+        this.ISBN = "0000";
+        this.disponible = true;
+    }
+
+    public Libro(String titulo, String autor, String ISBN, boolean disponible) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.ISBN = ISBN;
+        this.disponible = disponible;
+    }
+
+    public String getTitulo() { return titulo; }
+    public void setTitulo(String titulo) { this.titulo = titulo; }
+
+    public String getAutor() { return autor; }
+    public void setAutor(String autor) { this.autor = autor; }
+
+    public String getISBN() { return ISBN; }
+    public void setISBN(String ISBN) { this.ISBN = ISBN; }
+
+    public boolean isDisponible() { return disponible; }
+    public void setDisponible(boolean disponible) { this.disponible = disponible; }
+
+    public boolean estaDisponible() {
+        return disponible;
+    }
+    @Override
+    public String toString() {
+        return "Título: " + titulo +", Autor: " + autor + ", ISBN: " + ISBN + ", Disponible: " + (disponible ? "Sí" : "No");
+    }
+
 }
+    
